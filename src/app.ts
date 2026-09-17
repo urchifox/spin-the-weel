bb06161133f7129e86f2caa1ae5afb1bd5d5b80f
urchifox <126313096+urchifox@users.noreply.github.com>
2026-09-17 17:01:23 +0100

    mount spinning wheel in app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
+import { prizes } from "./prizes/prizes"
+import { SpinningWheel } from "./spinning-wheel"
 import "./styles/index.css"
 
 const root = document.querySelector("#app")
 
-if (root !== null) {
-	root.textContent = "Spin the weel"
+if (root !== null && root instanceof HTMLElement) {
+	new SpinningWheel({
+		root,
+		prizes,
+		wheelColors: { hueStart: 210, hueEnd: 300, saturation: 85, lightness: 60 },
+	})
 }
