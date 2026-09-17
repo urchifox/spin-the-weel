e27d5d527639f0ff03080e09c2a23bbc37218452
urchifox <126313096+urchifox@users.noreply.github.com>
2026-09-17 17:00:49 +0100

    add prizes info

---
+++ src/prizes/prizes.ts
@@ -0,0 +1,39 @@
+import bonusCash from "./images/bonusCash.png"
+import freeSpins from "./images/freeSpins.png"
+import depositMatchBonus from "./images/depositMatchBonus.png"
+import mysteryReward from "./images/mysteryReward.png"
+
+export const prizes = [
+	{
+		id: "bonusCash",
+		name: "Bonus Cash",
+		image: bonusCash,
+		callback: () => {
+			console.log("Bonus Cash")
+		},
+	},
+	{
+		id: "freeSpins",
+		name: "Free Spins",
+		image: freeSpins,
+		callback: () => {
+			console.log("Free Spins")
+		},
+	},
+	{
+		id: "depositMatchBonus",
+		name: "Deposit Match Bonus",
+		image: depositMatchBonus,
+		callback: () => {
+			console.log("Deposit Match Bonus")
+		},
+	},
+	{
+		id: "mysteryReward",
+		name: "Mystery Reward",
+		image: mysteryReward,
+		callback: () => {
+			console.log("Deposit Match Bonus")
+		},
+	},
+]
