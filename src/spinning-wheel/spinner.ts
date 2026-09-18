44efbcd88ad34e898af0c8584b4c04faee0c6004
urchifox <126313096+urchifox@users.noreply.github.com>
2026-09-18 12:44:17 +0100

    slow down start and end of spinning

--- src/spinning-wheel/spinner.ts
+++ src/spinning-wheel/spinner.ts
@@ -3,7 +3,7 @@ import { SpinningWheelMountProps } from "./types"
 
 export class Spinner {
 	private prizes: SpinningWheelMountProps["prizes"] = []
-	private readonly minTime = 1000
+	private readonly minTime = 1500
 	private readonly maxTime = 3000
 
 	setProps(props: Pick<SpinningWheelMountProps, "prizes">) {
@@ -13,12 +13,20 @@ export class Spinner {
 	clear() {}
 
 	spin(wheel: HTMLElement) {
-		wheel.classList.add("spinning-wheel__wheel--rolling")
+		wheel.classList.add("spinning-wheel__wheel--spinning")
 		const time = getRandomInteger({ min: this.minTime, max: this.maxTime })
 		setTimeout(() => {
-			wheel.classList.remove("spinning-wheel__wheel--rolling")
+			wheel.classList.remove("spinning-wheel__wheel--spinning")
 			requestAnimationFrame(() => {
-				this.onStop(wheel)
+				const wheelAngle = this.getCurrentWheelAngle(wheel)
+				wheel.style.setProperty("--wheel-end-angle", `${wheelAngle}deg`)
+				wheel.classList.add("spinning-wheel__wheel--stopped")
+				requestAnimationFrame(() => {
+					wheel.style.setProperty("--wheel-end-angle", `${wheelAngle + 360}deg`)
+					wheel.ontransitionend = () => {
+						this.onStop(wheel)
+					}
+				})
 			})
 		}, time)
 	}
@@ -33,15 +41,21 @@ export class Spinner {
 		console.log(prize.name)
 	}
 
-	private getWheelSegmentIndex(wheel: HTMLElement) {
+	private getCurrentWheelAngle(wheel: HTMLElement) {
 		const style = window.getComputedStyle(wheel)
 		const matrix = new DOMMatrixReadOnly(style.transform)
 		const angle = Math.atan2(matrix.b, matrix.a) * (180 / Math.PI)
+
 		const normalizedAngle = (angle + 360) % 360
+		return normalizedAngle
+	}
+
+	private getWheelSegmentIndex(wheel: HTMLElement) {
+		const wheelAngle = this.getCurrentWheelAngle(wheel)
 
 		const segmentsCount = this.prizes.length
 		const segmentAngle = 360 / segmentsCount
-		const shifted = (normalizedAngle + segmentAngle / 2) % 360
+		const shifted = (wheelAngle + segmentAngle / 2) % 360
 
 		const clockWiseIndex = Math.floor(shifted / segmentAngle)
 		const index = clockWiseIndex === 0 ? 0 : segmentsCount - clockWiseIndex
