8c39d3d1cd553514ea6a8de4a46b4f29e58947a0
urchifox <126313096+urchifox@users.noreply.github.com>
2026-09-19 18:05:33 +0100

    add mock server adapter

---
+++ src/mock-server-adapter/index.ts
@@ -0,0 +1,62 @@
+import { mockPrizes } from "../mock-prizes"
+import { getRandomItem } from "../spinning-wheel/helpers"
+import { Authorizer, Prize, WheelId } from "../spinning-wheel/types"
+
+type WheelInfo = {
+	prizes: Array<Prize>
+	claimedPrizeId: Prize["id"] | null
+}
+
+const defaultWheelInfo: WheelInfo = {
+	prizes: mockPrizes,
+	claimedPrizeId: null,
+}
+
+export class MockServerAdapter implements Authorizer {
+	private wheelsMap = new Map<WheelId, WheelInfo>()
+
+	private getWheelInfo(id: WheelId) {
+		const wheelInfo = this.wheelsMap.get(id)
+		if (wheelInfo === undefined) {
+			const newWheelInfo = { ...defaultWheelInfo }
+			this.wheelsMap.set(id, newWheelInfo)
+			return newWheelInfo
+		}
+
+		return wheelInfo
+	}
+
+	getInitialInfo(id: WheelId) {
+		const { prizes, claimedPrizeId } = this.getWheelInfo(id)
+
+		return Promise.resolve({
+			status: "success" as const,
+			claimedPrizeId: claimedPrizeId,
+			prizes: [...prizes],
+		})
+	}
+
+	requestSpin(id: WheelId) {
+		const wheelInfo = this.getWheelInfo(id)
+		const { prizes, claimedPrizeId } = wheelInfo
+		const wasSpun = claimedPrizeId !== null
+
+		const prizeId = claimedPrizeId ?? getRandomItem(prizes)?.id
+		if (prizeId === undefined) {
+			return Promise.resolve({
+				status: "error" as const,
+				error: "No prizes available",
+			})
+		}
+
+		if (!wasSpun) {
+			wheelInfo.claimedPrizeId = prizeId
+		}
+
+		return Promise.resolve({
+			status: "success" as const,
+			wasSpun: wasSpun,
+			prizeId: prizeId,
+		})
+	}
+}
--- src/spinning-wheel/types.ts
+++ src/spinning-wheel/types.ts
@@ -1,3 +1,22 @@
+export type WheelId = string
+
+export type Authorizer = {
+	getInitialInfo: (id: WheelId) => Promise<
+		| {
+				status: "success"
+				claimedPrizeId: Prize["id"] | null
+				prizes: Array<Prize>
+		  }
+		| { status: "error"; error: string }
+	>
+	requestSpin: (
+		id: WheelId
+	) => Promise<
+		| { status: "success"; wasSpun: boolean; prizeId: Prize["id"] }
+		| { status: "error"; error: string }
+	>
+}
+
 export type SpinningWheelMountProps = {
 	root: HTMLElement
 	prizes: Array<Prize>
